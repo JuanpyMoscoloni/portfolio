048f2f0bacaf400c3f89dce477ce24b67b2319d0
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-06-26 20:35:30 -0300

    update

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -39,7 +39,7 @@ export const Works = () => {
     <EffectScroll>
       <section
         id="works"
-        className="containerBox z-20 flex flex-col h-full w-full pt-[6rem] gap-4 items-center justify-center"
+        className="containerBox z-20 flex flex-col h-full w-full pt-[11rem] max-lg:pt-[6rem] gap-4 items-center justify-center"
       >
         <h1 className="mb-16 text-center text-5xl font-light">Trabajos</h1>
         <div className="w-full h-fit flex max-lg:flex-col items-center gap-8 justify-center">
