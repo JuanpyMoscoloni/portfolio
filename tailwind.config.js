99dd9e131e62609a357e015a3133797c1a4c6ee2
Juanpy <109622347+JuanpyMoscoloni@users.noreply.github.com>
2023-07-12 18:55:20 -0300

    Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter"],
-      },
       colors: {
         colorPage: "#D2C2B0",
         colorCard: "#FDEAD5",
